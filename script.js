cff1d95da4085069cda31bff0c28def6f30b366c
kashifjavaid1 <gmdcf797@gmail.com>
2023-03-22 17:30:02 +0500

    all javascript work js file

---
+++ script.js
@@ -0,0 +1,39 @@
+// var Name=10;
+// console.log("Result",Name);
+// var nameAndFatherName="kashif";
+// console.log(nameAndFatherName);
+// JAVASCRIPT IS CASE SENSETIVE
+// var num=5;
+// var NUM=20;
+
+// console.log(NUM);
+
+// var kashif=4;
+// console.log("value"+kashif);
+
+// concationation variable
+// var gc=20.25;
+// console.log("floting value" +gc);
+// var sb="jashof"
+// console.log("kashif"+ sb);
+
+// operators
+// console.log(4+4);
+// console.log(14-4);
+// console.log(4*4);
+// console.log(4/4);
+
+// var num1=40;
+// var num2=31;
+// console.log(num1+num2);
+// console.log(num1-num2);
+
+
+// var total="2"+4-2/2;
+// console.log(total);
+var temc=40;
+var temc=prompt("Enter user Value");
+var temf=(temc*9/5) + 32;
+console.log("tempur",temf, typeof(temf));
+
+
